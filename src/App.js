ea754006d23c3f9dd184297fda12d41c25290508
Bilal-hajM <bblal1003@gmail.com>
2023-06-24 02:16:52 +0300

    done

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import {db} from "./firebase";
 import { query, collection, onSnapshot, updateDoc, doc, addDoc, deleteDoc} from "firebase/firestore";
 
 const style = {
-  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+  bg: `h-screen w-screen p-4 bg-gradient-to-l from-[#737373] to-[#fafafa]`,
   container: 'bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4',
-  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  heading: `text-3xl font-bold text-center text-[#737373] p-2`,
   form: `flex justify-between`,
-  input: `border p-2 w-full text-xl`,
-  button: `border p-4 ml-2 bg-purple-500 text-slate-100 `,
+  input: `border p-2 w-full text-xl rounded-md`,
+  button: `border p-2 ml-2 text-slate-100 bg-[#737373] rounded-md `,
   count: `text-center p-2 `
 }
 
@@ -61,7 +61,7 @@ const deleteTodo = async (id) => {
      <div className={style.container}>
       <h3 className={style.heading}>Todo list</h3>
       <form onSubmit={createTodo} className={style.form}>
-        <input value={input} onChange={(e) => setInput(e.target.value)} className={style.input} type="text"/>
+        <input value={input} placeholder="Add a Todo item" onChange={(e) => setInput(e.target.value)} className={style.input} type="text"/>
         <button className={style.button}><AiOutlinePlus size={30}/></button>
       </form>
       <ul>
